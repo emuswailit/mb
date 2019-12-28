e90fcfeda06f7824a2bc0df1ef295fbfadf92055
omaria@mobiticket.co.ke <omaria@mobiticket.co.ke>
2019-12-28 23:34:03 +0300

    Working sending email with SendGrid API code

---
+++ PythonTutorials/news/users/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth import get_user_model
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+      model = get_user_model()
+      fields = UserCreationForm.Meta.fields + ('age',)
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = get_user_model()
+        fields = UserChangeForm.Meta.fields
+
+
+
+
